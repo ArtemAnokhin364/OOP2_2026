0c1845a2b461c43b16780407ae402c130c170356
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-03-03 19:22:24 +0100

    punkty i odcinki

--- svg/src/Main.java
+++ svg/src/Main.java
@@ -1,9 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Point p1 = new Point();
+        p1.x = 50.0;
+        p1.y = 50.0;
+        System.out.println(p1);
+        System.out.println(p1.toSvg());
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        p1.translate(10, -20);
+        System.out.println(p1);
+        System.out.println(p1.toSvg());
+
+        Point p2 = p1.translated(-30, 40);
+        System.out.println(p2);
+        System.out.println(p2.toSvg());
+
+        Segment s1 = new Segment();
+        s1.a = p1;
+        s1.b = p2;
+        System.out.println("długość s1 = " + s1.length());
+        System.out.println(s1);
+        System.out.println(s1.toSvg());
+
+        // TODO: Zadanie 5 na aktywność przy tablicy
     }
 }
