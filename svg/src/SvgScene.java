e682180e5fed167f82a976424f323665140d21e1
rniedziolkad <rafal.niedziolka-domanski@mail.umcs.pl>
2026-03-17 17:48:21 +0100

    dodano SvgScene

---
+++ svg/src/SvgScene.java
@@ -0,0 +1,16 @@
+public class SvgScene {
+    private Polygon[] polygons;
+    private int i;  // index pod który wstawić kolejny obiekt
+
+    public SvgScene() {
+        polygons = new Polygon[3];
+        i = 0;
+    }
+
+    public void addPolygon(Polygon poly) {
+        polygons[i] = poly;
+        i = (i + 1) % 3; // 0 -> 1 -> 2 -> 0 -> 1 -> ...
+    }
+
+
+}
